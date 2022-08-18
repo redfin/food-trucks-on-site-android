94b37d554cd7f8e9df1cccec53a12135107b4861
Joey McCoy <joey.mccoy@redfin.com>
2022-08-18 12:17:09 -0700

    added empty view-based activity as an option

---
+++ foodTrucks/app/src/main/java/com/redfinterviews/foodtrucks/MainActivityXml.kt
@@ -0,0 +1,11 @@
+package com.redfinterviews.foodtrucks
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivityXml : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
